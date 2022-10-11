02b5650e710a9e2d3ebe319f48a0e95973b8650e
r_hasan <rakibul.hasan@trucklagbe.com>
2022-10-12 05:47:31 +0600

    fetch all messages and structure for one to many communication

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+
+interface History {
+  receiverName: string,
+  senderName: string,
+  msg: string,
+  seenStatus: number,
+  datetime: Date
+}
 
 @Component({
   selector: 'app-chat',
@@ -10,7 +20,24 @@ export class ChatComponent implements OnInit {
   message: FormControl = new FormControl('');
   msg: string = "";
   finalMessage: string = "";
-  constructor() {
+  userName: string = "pet shop bd";
+  allMessage: any;
+  // public readonly messages$: Observable<Message[]>
+
+  constructor(private readonly afs: AngularFirestore) {
+    this.allMessage = afs
+    // .doc('/chat/dt-guest/history/merchant')
+    // .collection('/chat/dt-guest/history', ref => ref.where(afs, '==', this.userName))
+    .collection(`/chat/dt-guest/history/merchant/pet shop bd`, ref => ref.orderBy('datetime'))
+    .snapshotChanges()
+    .pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as History;
+        console.log(data.receiverName, ' is printed')
+        return { ...data };
+      }))
+    )
+
     this.message.valueChanges.subscribe(obserber => {
       this.msg = obserber;
     })
--- src/app/chat/p2p/p2p.component.ts
+++ src/app/chat/p2p/p2p.component.ts
@@ -1,9 +1,16 @@
 import { Component, Input, OnInit, SimpleChange } from '@angular/core';
 // import { Firestore, doc, docData, collection, getDocs, onSnapshot, query, orderBy, collectionData } from '@angular/fire/firestore';
-import { map, Observable } from 'rxjs';
+import { map, merge, Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 
 interface Message { id: string; message: string; date: Date; profile: string; type: string; url: string; }
+interface History {
+  receiverName: string,
+  senderName: string,
+  msg: string,
+  seenStatus: number,
+  datetime: Date
+}
 
 @Component({
   selector: 'app-p2p',
@@ -15,6 +22,7 @@ export class P2pComponent implements OnInit {
   
   public readonly messages$: Observable<Message[]>;
   public readonly messagesCollection$: AngularFirestoreCollection<Message>;
+  public readonly historyCollection$: AngularFirestoreCollection<History>;
 
   constructor(private readonly afs: AngularFirestore) {
     this.messagesCollection$ = afs.collection<Message>('/chat/dt-guest/room/p2p/pet shop bd-pet-1123', ref => ref.orderBy('date', 'asc'));
@@ -23,7 +31,8 @@ export class P2pComponent implements OnInit {
         const data = a.payload.doc.data() as Message;
         return { ...data };
       }))
-    )
+    );
+    this.historyCollection$ = afs.collection<History>('/chat/dt-guest/history');
   }
 
   ngOnChanges(changes: any) {
@@ -38,5 +47,7 @@ export class P2pComponent implements OnInit {
   storeMessage(text: string) {
     if (!text) return;
     this.messagesCollection$.add({message: text, date: new Date()} as Message);
+    this.historyCollection$.doc('merchant').collection('pet shop bd').doc('pet-1123').set({msg: text, receiverName: "pet-1124", senderName: "pet shop bd", datetime: new Date(), seenStatus: 1} as History, {merge : true});
+    this.historyCollection$.doc('client').collection('pet-1123').doc('pet shop bd').set({msg: text, receiverName: "pet-1124", senderName: "pet shop bd", datetime: new Date(), seenStatus: 1} as History, {merge : true});
   }
 }
